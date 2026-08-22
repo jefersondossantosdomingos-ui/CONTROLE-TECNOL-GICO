db478accf48ae8a8c996f0145d90a74de43c75b7
jefersondossantosdomingos-ui <jefersondossantosdomingos@gmail.com>
2026-08-21 22:26:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    mudança dela que faz o navegador perceber que há atualização.
    ============================================================ */
 
-const VERSAO = '2026-08-21.12e0cca3';
+const VERSAO = '2026-08-22.8e006e0e';
 const CACHE = 'controle-tecnologico-' + VERSAO;
 
 /* O que precisa estar guardado para o app abrir sem internet. */
